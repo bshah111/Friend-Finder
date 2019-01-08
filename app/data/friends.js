476f9454efa33d0d640b1d2f0ec381a513a6c439
Bhumi Shah <bshah11@outlook.com>
2019-01-07 17:24:01 -0700

    second push

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+var friends = [{
+    "name": "Ranbir Kapoor",
+    "photo": "https://medium.com/@Bollywoodirect/happy-birthday-ranbir-kapoor-12a66814176c",
+    "scores": [
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5"
+    ]
+    },{
+    "name": "Tom Cruise",
+    "photo": "https://www.biography.com/people/tom-cruise-9262645",
+    "scores": [
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    },{
+    "name": "Jennier Aniston",
+    "photo": "https://www.hellomagazine.com/profiles/jennifer-aniston/",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    }]
+
+    module.exports = friends;
+
